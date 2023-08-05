431814c7e70790ada63cc6750117f4ce1efe098b
CarlitoJuarez <carlito.b.juarez@gmail.com>
2023-08-05 13:18:45 +0200

    init

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/") # the '@' is a decorater
+def hello_world(): # define a function named hello_world
+    return 'hello'
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
