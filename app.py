cf6e43b7da8294e24e93a481768ba363657311e5
CarlitoJuarez <carlito.b.juarez@gmail.com>
2023-08-05 13:24:40 +0200

    index

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # the '@' is a decorater
 def hello_world(): # define a function named hello_world
-    return 'hello'
+    return render_template("index.html")
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
